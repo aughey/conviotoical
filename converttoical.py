33b93e9a7ffa7ea0d160f002f6efdddd3b7cb8b9
John Aughey <jha@aughey.com>
2011-07-29 09:54:15 -0500

    Getting previous 3 and next 12 months

--- converttoical.py
+++ converttoical.py
@@ -1,17 +1,17 @@
 from google.appengine.api import urlfetch
+from google.appengine.api import memcache
 from xml.dom import minidom
 import re
+import datetime
 
-url = 'https://secure3.convio.net/nmss/site/CROrgEventAPI?method=getMonthEvents&v=1.0&api_key=ZedRa4uc&response_format=xml&month=7&year=2011&interests=2623'
-
-result = urlfetch.fetch(url=url, headers={'use_intranet': 'yes'})
-
-dom = minidom.parseString(result.content)
-
-print 'Content-Type: text/plain'
-print ''
-print 'BEGIN:VCALENDAR'
-print 'VERSION:2.0'
+def getUrl(url):
+  cached = memcache.get(url)
+  if cached:
+    return cached
+  else:
+    data = urlfetch.fetch(url=url, headers={'use_intranet': 'yes'})
+    memcache.set(url,data,60 * 60) # Cache for 1 hour
+    return data
 
 def getChildren(element, childname):
   return [item for item in element.getElementsByTagName(childname) if item.parentNode == element]
@@ -50,19 +50,41 @@ def parseTime(date,time):
   return "%04d%02d%02dT%02d%02d00" % (year,month,day,hour,minute)
 
 
-for event in getChildren(getChild(dom,"getMonthEventsResponse"),'event'):
-  name = getChild(event,'name')
-  print 'BEGIN:VEVENT'
-  print "SUMMARY:%s" % getText(name.childNodes)
-  eventdate = getChild(event,'eventDate')
-  start = parseTime(getText(getChild(eventdate,'startDate')),getText(getChild(eventdate,'startTime')))
-  enddate = getChild(eventdate,'endDate')
-  if(enddate == None):
-    enddate = getChild(eventdate,'startDate')
+def printevents(month,year):
+  url = 'https://secure3.convio.net/nmss/site/CROrgEventAPI?method=getMonthEvents&v=1.0&api_key=ZedRa4uc&response_format=xml&month=%d&year=%d&interests=2623' % (month,year)
 
-  end = parseTime(getText(enddate),getText(getChild(eventdate,'endTime')))
-  print "DTSTART;TZID=US-Central:" + start
-  print "DTEND;TZID=US-Central:" + end
-  print 'END:VEVENT'
+  print "COMMENT:Result of date fetch %d-%d" % (year,month)
+  print "COMMENT:Url is %s" % url
+
+  result = getUrl(url)
+
+  dom = minidom.parseString(result.content)
+
+  for event in getChildren(getChild(dom,"getMonthEventsResponse"),'event'):
+    name = getChild(event,'name')
+    print 'BEGIN:VEVENT'
+    print "SUMMARY:%s" % getText(name.childNodes)
+    eventdate = getChild(event,'eventDate')
+    start = parseTime(getText(getChild(eventdate,'startDate')),getText(getChild(eventdate,'startTime')))
+    enddate = getChild(eventdate,'endDate')
+    if(enddate == None):
+      enddate = getChild(eventdate,'startDate')
+
+    end = parseTime(getText(enddate),getText(getChild(eventdate,'endTime')))
+    print "DTSTART;TZID=US-Central:" + start
+    print "DTEND;TZID=US-Central:" + end
+    print 'END:VEVENT'
+
+
+
+print 'Content-Type: text/plain'
+print ''
+print 'BEGIN:VCALENDAR'
+print 'VERSION:2.0'
+
+now = datetime.datetime.now();
+for monthoffset in range(-3,12):
+  toprint = now + datetime.timedelta(days=monthoffset * 30)
+  printevents(toprint.month,toprint.year)
 
 print 'END:VCALENDAR'
